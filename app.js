28335a1007d90a03c6b7141248b32e2ce94aba10
Adam Salinas <a_salinas1996@yahoo.com>
2024-04-29 16:37:41 -0700

    initiated pug and added public folder from natours project

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 
 const teamRouter = require('./routes/teamRoutes');
+// const viewRouter = require('./views/base.pug');
 
 const app = express();
+
+// setting up pug engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving Static Files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // middlewares
 if (process.env.NODE_END === 'development') {
@@ -12,7 +21,13 @@ if (process.env.NODE_END === 'development') {
 
 app.use(express.json());
 
-// routes
+// pug routes
+// app.use('/', viewRouter);
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
+// api routes
 app.use('/api/v1/teams', teamRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -16,6 +16,21 @@ mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 // starting server
 const port = process.env.PORT || 2000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
 });
